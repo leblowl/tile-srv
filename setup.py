c18acbb3f175106465acd500bd7fcb3192bbf1b0
Lucas Leblow <leblowl@gmail.com>
2015-07-29 16:18:36 -0700

    added setup.py, added namespace dir in src

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name = 'tile-srv',
+      version = '0.1.0',
+      classifiers = ['Programming Language :: Python :: 2.7'],
+      install_requires = ['Cython==0.22.1',
+                          'gevent==1.1b2.dev0',
+                          'greenlet==0.4.7',
+                          'wheel==0.24.0'])
