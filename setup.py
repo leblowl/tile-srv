e869a5df4fade5abf82901197c5ef1d983bad6ee
Lucas Leblow <leblowl@gmail.com>
2015-07-30 17:02:46 -0700

    tile-gen dep link using git+https && version specifier

--- setup.py
+++ setup.py
@@ -3,10 +3,10 @@ from setuptools import setup
 setup(name = 'tile-srv',
       version = '0.1.0',
       classifiers = ['Programming Language :: Python :: 2.7'],
-      dependency_links = ['https://github.com/leblowl/tile-gen#egg=tile-gen'],
+      dependency_links = ['git+https://github.com/leblowl/tile-gen#egg=tile-gen-0.1.0'],
       install_requires = ['Cython==0.22.1',
                           'bottle==0.12.8',
-                          'gevent==1.1b2.dev0',
+                          'gevent==1.1b1',
                           'greenlet==0.4.7',
                           'tile-gen==0.1.0',
                           'wheel==0.24.0'])
